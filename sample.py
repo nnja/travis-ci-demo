323b95f67dec439110931b5990ad7a37ff6ca47b
Nina Zakharenko <nina@nnja.io>
2017-10-01 22:45:34 -0700

    Add an example method and some unit tests

---
+++ sample.py
@@ -0,0 +1,7 @@
+"""
+Module that contains sample functions to unit test.
+"""
+
+def add(x, y):
+    """ Add two values together. """
+    return x + y
---
+++ test_sample.py
@@ -0,0 +1,25 @@
+import unittest
+
+from sample import add
+
+
+class SampleTest(unittest.TestCase):
+    """
+    Unit tests for code in the sample module
+    """
+
+    def test_add_integers(self):
+        result = add(3, 5)
+        self.assertEquals(8, result)
+
+    def test_add_strings(self):
+        result = add('Hello,', ' World!')
+        self.assertEquals('Hello, World!', result)
+
+    def test_add_lists(self):
+        result = add([3], [5])
+        self.assertEquals([3, 5], result)
+
+
+if __name__ == '__main__':
+    unittest.main()
